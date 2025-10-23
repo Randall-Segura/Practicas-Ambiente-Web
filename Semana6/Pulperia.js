fc65ded209705c337f5c9d80af239366e8d653bd
Randall-Segura <yjime01@gmail.com>
2025-10-23 14:30:00 -0600

    Cambios

--- Semana6/Pulperia.js
+++ Semana6/Pulperia.js
@@ -1,7 +1,29 @@
 document.addEventListener("DOMContentLoaded",function(){
     document.getElementById("sinProductos").style.display="block";
 
+    let clicks=0;
+    const facturacion=document.getElementById("facturacion");
+    const boton=document.getElementById("btn1");
+    const contadorClicks=document.getElementById("contador");
+
+    boton.addEventListener("click", function(){
+        clicks++;
+
+    })
+
+
+
+    document.querySelector(".btn.total").addEventListener("click", function(){
+        let PrecioKSI=document.getElementById("PrecioKSI").innerText;
+        let precio=parseInt(PrecioKSI) || 0;
+        let total=precio* clicks;
+        facturacion.innerText= total;
+    })
+
+
     
+
+
 
 
 
